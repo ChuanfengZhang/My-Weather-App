a13906cbfc9850349a2609e2e01d8479f3cb91c1
Chuanfeng <wfhtzcf@gmail.com>
2016-10-31 11:03:37 +0800

    rebuild

--- js/index.js
+++ js/index.js
@@ -1,26 +1,25 @@
-function findWeather() {
-    var cityUrl = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js';
-    $.getScript(cityUrl, function(script, textStatus, jqXHR) {
-        var citytq = remote_ip_info.city ;// 获取城市
-        var url = "http://php.weather.sina.com.cn/iframe/index/w_cl.php?code=js&city=" + citytq + "&day=0&dfc=3";
-        $.ajax({
-            url : url,
-            dataType : "script",
-            scriptCharset : "gbk",
-            success : function(data) {
-                var _w = window.SWther.w[citytq][0];
-                var _f= _w.f1+"_0.png";
-                if(new Date().getHours() > 17){
-                     _f= _w.f2+"_1.png";                
-                }
-                var img = "<img width='16px' height='16px' src='http://i2.sinaimg.cn/dy/main/weather/weatherplugin/wthIco/20_20/" +_f
-                + "' />";
-                var tq = citytq + " " + img + " " + _w.s1 + " " + _w.t1 + "℃～" + _w.t2 + "℃  " + _w.d1 + _w.p1 + "级";
-                 
-                $('#weather').html(tq);
-            }
-        });
+$(document).ready(function() {
+    var ip = returnCitySN["cip"];
+    var httpRequest = "https://api.heweather.com/x3/weather?cityip=" + ip + "&key=e2589cc3b44644679cbf74b72aafa0b9";
+    $.getJSON(httpRequest, function(json) {
+        var city = json["HeWeather data service 3.0"][0]["basic"]["city"];
+        var country = json["HeWeather data service 3.0"][0]["basic"]["cnty"];
+        var weather = json["HeWeather data service 3.0"][0]["now"]["cond"]["txt"];
+        var temp = json["HeWeather data service 3.0"][0]["now"]["tmp"];
+        $(".loc").html(city + "," + country);
+        $(".temp").html(temp + "&#8451");
+        $(".weather").html(weather);
+        var dailyForecast = json["HeWeather data service 3.0"][0]["daily_forecast"];
+        var htmlStr = "";
+        for (var i = 0; i < dailyForecast.length; i++) {
+            htmlStr += "<li class='forecast-ul-li'>";
+            htmlStr += "<ul class='forecast-ul-li-ul'>"
+            htmlStr += "<li class='forecast-ul-li-ul-li'>" + dailyForecast[i]["date"] + "</li>";
+            htmlStr += "<li class='forecast-ul-li-ul-li'>" + dailyForecast[i]["tmp"]["max"] + "-" + dailyForecast[i]["tmp"]["min"] + "</li>";
+            htmlStr += "<li class='forecast-ul-li-ul-li'>" + dailyForecast[i]["cond"]["txt_d"] + "-" + dailyForecast[i]["cond"]["txt_n"] + "</li>";
+            htmlStr += "</ul></li>";
+            $(".forecast-ul").html(htmlStr);
+        }
+
     });
-}
- 
-findWeather();
+});
